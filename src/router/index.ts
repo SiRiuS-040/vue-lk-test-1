44af7c905e23a26dcdd64710b8e7b97fbe6d37d5
voskresenskiy.v <vladislav.voskresenskiy@gmail.com>
2025-03-26 01:42:08 +0300

    +

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ import { loadLayoutMiddleware } from "@/router/middleware/loadLayoutMiddleware";
 import { checkAuth } from "@/router/middleware/auth";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/auth-login",
